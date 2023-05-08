9e11d45f3023c7c22e34670291dd75f0da7d1191
Ireneusz Miernik <irekmiernik@gmail.com>
2023-05-08 08:32:35 +0200

    Added object imagesTable with function

--- js/script.js
+++ js/script.js
@@ -1,43 +1,35 @@
 {
-    const imagesTable = [];
-
-    const setImagesTable = () => {
-        imagesTable.push({ link: "Zima w Kaniach.jpg", title: "Zima w Kaniach" });
-        imagesTable.push({ link: "Odległość z Kań do Warszawy Cetrum.jpg", title: "Odległość z Kań do Warszawy Cetrum" });
-        imagesTable.push({ link: "Stacja kolejki WKD w Kaniach-1.jpg", title: "Stacja kolejki WKD w Kaniach" });
-        imagesTable.push({ link: "Stacja kolejki WKD w Kaniach-2.jpg", title: "Stacja kolejki WKD w Kaniach" });
-        imagesTable.push({ link: "Stacja kolejki WKD w Kaniach-3.jpg", title: "Stacja kolejki WKD w Kaniach" });
+    const imagesTable = {
+        table: [],
+        imageNumber: -1,
+        nextImage() {
+            this.imageNumber++;
+            if (this.imageNumber >= this.table.length) this.imageNumber = 0;
+            return this.table[this.imageNumber];
+        }
     }
 
-    const setImage = (imageAccount) => {
-        const sectionImage = document.querySelector(".js-section__image");
-        const sectionHref = document.querySelector(".js-section__href");
-        const sectionImageTitle = document.querySelector(".section__imageTitle");
-
-        sectionImage.src = `images/${imageAccount.link}`;
-        sectionImage.alt = imageAccount.title;
-        sectionHref.href = `images/${imageAccount.link}`;
-        sectionImageTitle.innerText = imageAccount.title;
+    const inputImages = (table) => {
+        table.push({ link: "Zima w Kaniach.jpg", title: "Zima w Kaniach" });
+        table.push({ link: "Odległość z Kań do Warszawy Cetrum.jpg", title: "Odległość z Kań do Warszawy Cetrum" });
+        table.push({ link: "Stacja kolejki WKD w Kaniach-1.jpg", title: "Stacja kolejki WKD w Kaniach" });
+        table.push({ link: "Stacja kolejki WKD w Kaniach-2.jpg", title: "Stacja kolejki WKD w Kaniach" });
+        table.push({ link: "Stacja kolejki WKD w Kaniach-3.jpg", title: "Stacja kolejki WKD w Kaniach" });
     }
 
-    const clickCounter = {
-        counter: NaN,
-        setCounter(counter) { this.counter = counter },
-        getCounter() { return this.counter }
-    }
-
-    const onButtonClick = () => {
-        if (clickCounter.getCounter() < (imagesTable.length - 1)) clickCounter.setCounter(clickCounter.getCounter() + 1);
-        else clickCounter.setCounter(0);
-        setImage(imagesTable[clickCounter.getCounter()]);
+    const setNextImage = () => {
+        let image = imagesTable.nextImage();
+        document.querySelector(".js-section__image").src = `images/${image.link}`;
+        document.querySelector(".js-section__image").alt = image.title;
+        document.querySelector(".js-section__href").href = `images/${image.link}`;
+        document.querySelector(".section__imageTitle").innerText = image.title;
     }
 
     const init = () => {
-        setImagesTable();
-        clickCounter.setCounter(0);
-        setImage(imagesTable[0]);
+        inputImages(imagesTable.table);
+        setNextImage();
     }
 
     init();
-    document.querySelector(".section__button").addEventListener("click", onButtonClick);
+    document.querySelector(".section__button").addEventListener("click", setNextImage);
 }
