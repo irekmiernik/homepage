7e5e9cbbf3403e1c1f2ca7058f2cc0fb4744e102
Ireneusz Miernik <irekmiernik@gmail.com>
2023-05-16 13:15:54 +0200

    More clear JS

--- js/script.js
+++ js/script.js
@@ -3,22 +3,21 @@
         table: [],
         imageNumber: -1,
         nextImage() {
-            this.imageNumber++;
-            if (this.imageNumber >= this.table.length) this.imageNumber = 0;
+            this.imageNumber = this.imageNumber >= this.table.length - 1 ? 0 : ++this.imageNumber;
             return this.table[this.imageNumber];
         }
     }
 
     const inputImages = (container) => {
         container.push({ link: "Zima w Kaniach.jpg", title: "Zima w Kaniach" });
-        container.push({ link: "Odległość z Kań do Warszawy Cetrum.jpg", title: "Odległość z Kań do Warszawy Cetrum" });
+        container.push({ link: "Odległość z Kań do Warszawy Cetrum.jpg", title: "Odległość z Kań do centrum Warszawy" });
         container.push({ link: "Stacja kolejki WKD w Kaniach-1.jpg", title: "Stacja kolejki WKD w Kaniach" });
         container.push({ link: "Stacja kolejki WKD w Kaniach-2.jpg", title: "Stacja kolejki WKD w Kaniach" });
         container.push({ link: "Stacja kolejki WKD w Kaniach-3.jpg", title: "Stacja kolejki WKD w Kaniach" });
     }
 
     const showNextImage = () => {
-        let image = imagesContainer.nextImage();
+        const image = imagesContainer.nextImage();
         document.querySelector(".js-section__image").src = `images/${image.link}`;
         document.querySelector(".js-section__image").alt = image.title;
         document.querySelector(".js-section__href").href = `images/${image.link}`;
@@ -28,8 +27,9 @@
     const init = () => {
         inputImages(imagesContainer.table);
         showNextImage();
+        const nextImageButton = document.querySelector(".section__button");
+        nextImageButton.addEventListener("click", showNextImage);
     }
 
     init();
-    document.querySelector(".section__button").addEventListener("click", showNextImage);
 }
