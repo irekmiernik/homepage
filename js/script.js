e906864e0692251ac69248d662a6bd9a2769a619
Ireneusz Miernik <irekmiernik@gmail.com>
2023-05-17 16:41:11 +0200

    Version without object (very simple)

--- js/script.js
+++ js/script.js
@@ -1,31 +1,23 @@
 {
-    const imagesContainer = {
-        table: [],
-        imageNumber: -1,
-        nextImage() {
-            this.imageNumber = this.imageNumber >= this.table.length - 1 ? 0 : ++this.imageNumber;
-            return this.table[this.imageNumber];
-        }
-    }
-
-    const inputImages = (container) => {
-        container.push({ link: "Zima w Kaniach.jpg", title: "Zima w Kaniach" });
-        container.push({ link: "Odległość z Kań do Warszawy Cetrum.jpg", title: "Odległość z Kań do centrum Warszawy" });
-        container.push({ link: "Stacja kolejki WKD w Kaniach-1.jpg", title: "Stacja kolejki WKD w Kaniach" });
-        container.push({ link: "Stacja kolejki WKD w Kaniach-2.jpg", title: "Stacja kolejki WKD w Kaniach" });
-        container.push({ link: "Stacja kolejki WKD w Kaniach-3.jpg", title: "Stacja kolejki WKD w Kaniach" });
-    }
+    const imagesTable = [
+        { link: "images/Zima w Kaniach.jpg", title: "Zima w Kaniach" },
+        { link: "images/Odległość z Kań do Warszawy Cetrum.jpg", title: "Odległość z Kań do centrum Warszawy" },
+        { link: "images/Stacja kolejki WKD w Kaniach-1.jpg", title: "Stacja kolejki WKD w Kaniach" },
+        { link: "images/Stacja kolejki WKD w Kaniach-2.jpg", title: "Stacja kolejki WKD w Kaniach" },
+        { link: "images/Stacja kolejki WKD w Kaniach-3.jpg", title: "Stacja kolejki WKD w Kaniach" }
+    ];
+    let imageNumber = -1;
 
     const showNextImage = () => {
-        const image = imagesContainer.nextImage();
-        document.querySelector(".js-section__image").src = `images/${image.link}`;
+        imageNumber = imageNumber >= imagesTable.length - 1 ? 0 : ++imageNumber;
+        const image = imagesTable[imageNumber];
+        document.querySelector(".js-section__image").src = image.link;
         document.querySelector(".js-section__image").alt = image.title;
-        document.querySelector(".js-section__href").href = `images/${image.link}`;
+        document.querySelector(".js-section__href").href = image.link;
         document.querySelector(".section__imageTitle").innerText = image.title;
     }
 
     const init = () => {
-        inputImages(imagesContainer.table);
         showNextImage();
         document.querySelector(".section__button").addEventListener("click", showNextImage);
     }
